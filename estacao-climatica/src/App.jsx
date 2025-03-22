665b11023e7db6da20f47d7dca1db2ec6863c18d
Rafael Brito <rafaelbc2005@gmail.com>
2025-03-21 22:04:41 -0300

    Encerra a apostila, ciclo de vida de componentes react

--- estacao-climatica/src/App.jsx
+++ estacao-climatica/src/App.jsx
@@ -1,19 +1,45 @@
 import React from 'react'
- 
+import EstacaoClimatica from './EstacaoClimatica'
+import Loading from './Loading'
+
 class App extends React.Component {
- 
-  constructor(props){
-    super(props)
-    this.state = {
-      latitude: null,
-      longitude: null,
-      estacao: null,
-      data: null,
-      icone: null,
-      mensagemDeErro: null
-    }
+
+  // constructor(props){
+  //   super(props)
+  // this.state = {
+  //   latitude: null,
+  //   longitude: null,
+  //   estacao: null,
+  //   data: null,
+  //   icone: null,
+  //   mensagemDeErro: null
+  //   }
+  //   console.log('constructor')
+  // }
+
+  state = {
+    latitude: null,
+    longitude: null,
+    estacao: null,
+    data: null,
+    icone: null,
+    mensagemDeErro: null
   }
- 
+
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.obterLocalizacao()
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+
   obterEstacao = (data, latitude) => {
     const anoAtual = data.getFullYear()
     //21/06 (contagem do mês começa do zero)
@@ -25,22 +51,22 @@ class App extends React.Component {
     //21/03
     const d4 = new Date(anoAtual, 2, 21)
     const estaNoSul = latitude < 0
-    if(data >= d1 && data < d2)
+    if (data >= d1 && data < d2)
       return estaNoSul ? 'Inverno' : 'Verão'
-    if(data >= d2 && data < d3)
+    if (data >= d2 && data < d3)
       return estaNoSul ? 'Primavera' : 'Outono'
-    if(data >= d3 || data < d1)
+    if (data >= d3 || data < d4)
       return estaNoSul ? 'Verão' : 'Inverno'
     return estaNoSul ? 'Outono' : 'Primavera'
   }
- 
+
   icones = {
     'Primavera': 'flower',
     'Verão': 'umbrella-beach',
     'Outono': 'leaf',
     'Inverno': 'snowflake'
   }
- 
+
   obterLocalizacao = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
@@ -61,61 +87,42 @@ class App extends React.Component {
       error => {
         this.setState({
           mensagemDeErro: 'Tente novamente mais tarde'
-        })  
+        })
       }
     )
   }
- 
- 
-  
-  render(){
-    console.log(this.state)
+
+  render() {
+    // console.log(this.state)
     console.log('render')
     return (
       <div className="container mt-2">
         <div className="row justify-content-center">
           <div className="col-sm-12 col-md-8 col-xxl-6">
-            <div className="card">
-              <div className="card-body">
-                
-                <div
-                  className="d-flex align-items-center border rounded mb-2 p-5"
-                  style={{height: '10rem'}}>
-                    <i className={`fas fa-5x fa-${this.state.icone}`}></i>
-                    <p className="w-75 ms-3 text-center fs-1">
-                      {this.state.estacao}
-                    </p>                
-                </div>
- 
-                <div>
-                  {/* renderização condicional */}
-                  <p className="text-center">
-                    {
-                      this.state.latitude ?
-                        `Coordenadas: ${this.state.latitude},${this.state.longitude}. Data: ${this.state.data}.`
-                      :
-                      this.state.mensagemDeErro ?
-                        `${this.state.mensagemDeErro}`
-                      :
-                        'Clique no botão para saber a sua estação climática'
-                    }
-                  </p>                    
-                </div>
- 
-                {/* button.btn.btn-outline-primary.w-100.mt-2{Qual a minha estação?} */}
-                <button
-                  onClick={this.obterLocalizacao}
-                  className="btn btn-outline-primary w-100 mt-2">
-                    Qual a minha estação?
-                </button>
-                
-              </div>
-            </div>  
+            {
+              (! this.state.latitude && !this.state.mensagemDeErro) ?
+              <Loading
+              mensagem="por favor responda a mensagem de solicitação de localização" />
+              :
+              this.state.mensagemDeErro ?
+                <p className="border rounded p-2 fs-1 textcenter">
+                  É preciso permitir acesso a localização para que seja possível mostrar a sua estação climática
+                </p>
+                :
+                <EstacaoClimatica
+                  icone={this.state.icone}
+                  estacao={this.state.estacao}
+                  latitude={this.state.latitude}
+                  longitude={this.state.longitude}
+                  data={this.state.data}
+                  mensagemDeErro={this.state.mensagemDeErro}
+                  obterLocalizacao={this.obterLocalizacao} />
+            }
           </div>
         </div>
       </div>
     )
   }
 }
- 
+
 export default App
